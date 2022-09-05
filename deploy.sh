7e0b0c13327da93f9992d86e74a3439ccc1784fe
qiuxchao <2915241235@qq.com>
2022-09-05 09:53:16 +0800

    change deploy pwd

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 yarn docs:build
 ssh root@120.48.59.112 "clear_blog.sh"
-scp -r /Users/mac/Desktop/DemoCode/MyDocument/docs/.vuepress/dist/* root@120.48.59.112:/www/html/
+scp -r $PWD/docs/.vuepress/dist/* root@120.48.59.112:/www/html/
